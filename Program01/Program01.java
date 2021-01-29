250f737698d5738ed73b34ee916543c13596f884
sam kersebet <sam.kersebet@gmail.com>
2021-01-28 19:06:54 -0600

    got the program to output random dealt cards with no duplicates but there is a bug with spacing now

--- Program01/Program01.java
+++ Program01/Program01.java
@@ -1,10 +1,8 @@
 
+
 public class Program01{
 
     public static void main(String [] args){
-
-        String [] numberDeck = {"1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","1H","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC"}; 
-
 
         boolean[] myDeck = new boolean[52];
         final int cardsPerRow = 8;
@@ -33,7 +31,7 @@ public class Program01{
            deck[i] = true;
        }
     }
-    public static void emptyDeck(boolean [] deck){
+    public static boolean emptyDeck(boolean [] deck){
         for(int i = 0; i < deck.length; i++){
             if(deck[i] == true){
                 return false;
@@ -42,15 +40,18 @@ public class Program01{
         return true;
     }
     public static int dealCard(boolean [] deck){
-        int cardNum = Math.random() * 52;
+        int cardNum = (int)Math.floor(Math.random() * 52);
         if(deck[cardNum] == true){ 
-            deck[dealCount] = false;
+            deck[cardNum] = false;
             return cardNum;
-        } else{
-            dealCard(myDeck);
         }
-    }
+        return -1;
+     }
     public static void printCard(int card){
-        System.out.print(numberDeck(card));
+    	 String [] numberDeck = {"1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","1H","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC"}; 
+    	if(card != -1) {
+            System.out.print(numberDeck[card]);
+        }
+        
     }
 }
