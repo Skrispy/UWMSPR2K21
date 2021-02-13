386243a0875307e30458289854ac9c3cd2a23fd8
sam kersebet <sam.kersebet@gmail.com>
2021-02-13 15:22:59 -0600

    start program03

---
+++ Program03/card.java
@@ -0,0 +1,10 @@
+public class Card{
+    private String suit = "Clubs";
+    private int rank = 0;
+
+    public String clone(){
+        return this.suit + String() this.rank = rank;
+    }
+    public boolean equals(Card guest)
+
+}
