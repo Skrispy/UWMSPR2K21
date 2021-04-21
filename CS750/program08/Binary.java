100494629297cbc210ecbbb2c80a579c0f241072
sam kersebet <sam.kersebet@gmail.com>
2021-04-21 15:11:00 -0500

    layed foundation for program08

---
+++ CS750/program08/Binary.java
@@ -0,0 +1,16 @@
+public class Binary{
+    public Binary()
+    // encode a non-negative (base 10) number val such into
+    // the host’s bitstring - used in the 
+    
+    Binary(long val) constructor
+    private void encode(long val)
+    public Binary(long val)
+    public Binary(BitString guest)
+    public Binary(Binary guest)
+    public Binary clone()
+    // return a new Binary number that is the result of adding the
+    // host bitstring to the guest bitstring under then assumption
+    // that both are representing (base 2) numbers
+    public Binary addition(Binary guest)
+}
