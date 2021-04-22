53612aa91b88b79b66e132f43d86384c839e966f
Sam Kersebet <sam.kersebet@gmail.com>
2021-04-22 15:03:20 -0500

    built out abstract bit

--- CS750/program08/AbstractBit.java
+++ CS750/program08/AbstractBit.java
@@ -1,16 +1,41 @@
 public abstract class AbstractBit{
 
 private boolean bit;
+
 public abstract AbstractBit clone();
 public abstract AbstractBit addBits(AbstractBit guest);
 public abstract AbstractBit addBits(AbstractBit guest1, AbstractBit guest2);
 public abstract AbstractBit carryBit(AbstractBit guest);
 public abstract AbstractBit carryBit(AbstractBit guest1, AbstractBit guest2);
-protected void setBit(boolean value)
-public boolean getBit()
-public AbstractBit()
-public AbstractBit(boolean value)
-public AbstractBit(AbstractBit guest)
-public boolean equals(AbstractBit guest)
-public String toString()
+protected void setBit(boolean value){
+    this.bit = value;
+}
+public boolean getBit(){
+    return this.bit;
+}
+public AbstractBit(){
+    this();
+}
+public AbstractBit(boolean value){
+    super();
+    this.setBit(value);
+}
+public AbstractBit(AbstractBit guest){
+    this(guest.getBit());
+}
+public boolean equals(AbstractBit guest){
+
+    if ( guest == null )
+    return false;
+  if ( guest == this )
+    return true;
+  if ( !(guest instanceof AbstractBit)) 
+    return false; 
+  
+  AbstractBit tg = (AbstractBit)guest;
+  return this.getBit() == tg.getBit();
+}
+public String toString(){
+    return ""+this.getBit();
+}
 }
--- CS750/program08/BinaryBit.java
+++ CS750/program08/BinaryBit.java
@@ -1,9 +1,17 @@
 public class BinaryBit extends AbstractBit{
+
     public static final BinaryBit zero = new BinaryBit(false);
     public static final BinaryBit one = new BinaryBit(true);
-    public BinaryBit()// allows construction with a boolean - false->0, true->1
-    public BinaryBit(boolean bit)// allows construction with an int - should be 0 or 1
-    public BinaryBit(int bit)
+    
+    public BinaryBit(){// allows construction with a boolean - false->0, true->1
+        this();
+    }
+    public BinaryBit(boolean bit){// allows construction with an int - should be 0 or 1
+        super(bit);
+    }
+    public BinaryBit(int bit){
+        
+    }
     public BinaryBit(BinaryBit guest)
     public BinaryBit clone()
     public boolean equals(BinaryBit guest)
@@ -12,4 +20,5 @@ public class BinaryBit extends AbstractBit{
     public AbstractBit addBits(AbstractBit guest1, AbstractBit guest2)// returns the high order bit of adding the host bit to the guest bit
     public AbstractBit carryBit(AbstractBit guest)// returns the high order bit of adding the host bit to the guest bit
     public AbstractBit carryBit(AbstractBit guest1, AbstractBit guest2)
+
 }
