ddd851ac586fee57228350488cda9e5328db16a0
sam kersebet <sam.kersebet@gmail.com>
2021-05-15 20:17:11 -0500

    starting 411 final

--- CS411/FinalProject/final.py
+++ CS411/FinalProject/final.py
@@ -7,4 +7,7 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropou
 from matplotlib import pyplot
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
 
+data = ImageDataGenerator()
+train = data.flow_from_directory('/Data/Train',classes=["Blank","Fist","Five","ThumbsUp","Two","Yo"] class_mode="categorical",target_size=(200,200))
